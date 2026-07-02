c2204d28046fbc73574957bcc400d860c0f8ba26
Ramon <pcnay2003@outlook.com>
2026-07-01 18:15:31 -0700

    Se termina el capitulo de Generar URL y agregar parametros a la URL de forma dinamica

---
+++ Utilitario/Utilitario.go
@@ -0,0 +1,38 @@
+package utilitario
+
+import (
+// "log"
+// "net/url" // Usado para  agregar URL
+)
+
+/*
+func Generar_url(parametro, host, protocolo string, url_generar map[string]string) string {
+	// Se agrega los datos de la ruta.
+	url, err := url.Parse("/carrito")
+
+	if err != nil {
+		panic("Ocurrio Un Error")
+	}
+	url.Host = host
+	url.Scheme = protocolo
+
+	// Agregando los parametros.
+	nuevo_parametro := url.Query()
+
+	for key, value := range url_generar {
+		nuevo_parametro.Add(key, value)
+
+	}
+	//nuevo_parametro.Add("id", "5")
+	//nuevo_parametro.Add("nombre", "Teclados")
+
+	// Agregando a la URL el nuevo parametro creado.
+	url.RawQuery = nuevo_parametro.Encode()
+
+	// Imprime la url modificada
+	log.Println(url.String())
+
+	return url.String()
+
+} // func generar_url(parametro, host, pro
+*/
--- main.go
+++ main.go
@@ -2,17 +2,71 @@ package main
 
 import (
 	"fmt"
+	//"github.com/DevOps-PcNay/Go_Licito/Utilitario"
 	"log"
-	"net/http"
+	"net/http" // Servidor Web
+	"net/url"  // Usado para generar la URL
 )
 
 func main() {
 	// Definiendo la ruta principal
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Mi pagina Principal, utilizando Air para autoejecutarse ")
+
+		// Creando la ruta.
+		// Se agrega los datos de la ruta.
+		host := "www.my-empresa.com" // "localhost:3034"
+		protocolo := "http"
+		ruta_nueva := "/carrito"
+		parametros := map[string]string{"id": "1", "nombre": "Galleta"}
+		Generar_url(ruta_nueva, host, protocolo, nil)
+
+		log.Println(Generar_url(ruta_nueva, host, protocolo, parametros))
+
+		/*
+
+			// url.Parse ; url = Es un paquete de Go.
+			// Creando la URL
+			ruta_nueva, err := url.Parse("/carrito")
+
+			if err != nil {
+				panic("Ocurrio Un Error")
+			}
+
+			// Definiendo las opciones requeridas para crear la "ruta"
+			ruta_nueva.Host = host
+			ruta_nueva.Scheme = protocolo
+
+			// Agregando los parametros.
+			nuevo_parametro := ruta_nueva.Query()
+			nuevo_parametro.Add("id", "5")
+			nuevo_parametro.Add("nombre", "Teclados")
+
+			// Agregando a la URL el nuevo parametro creado.
+			ruta_nueva.RawQuery = nuevo_parametro.Encode()
+
+			// Imprime la URL con los parmetros
+			log.Println(ruta_nueva.String())
+		*/
+
 	})
+
 	http.HandleFunc("/productos", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Pagina De Productos")
+		// Para imprimir la URL pero sin el dominio.
+		log.Println("Obteniendo la URL ", r.URL)
+		log.Println("Obteniendo los parametros ", r.URL.RawQuery)
+		// Obteniendo el valor de las variables que se envian en los parametros
+		log.Println("Id Producto = ", r.URL.Query().Get("idProduct"))
+		log.Println("Nombre = ", r.URL.Query().Get("nombre"))
+
+		// Agregando una variable de http: precio = 10
+		// Se utilizan los mapas
+		nuevo_valor := r.URL.Query()
+		nuevo_valor.Add("precio", "10")
+		log.Println("Contenido Variable Http nueva ", nuevo_valor.Encode())
+
 	})
 
 	http.HandleFunc("/categorias", func(w http.ResponseWriter, r *http.Request) {
@@ -35,3 +89,33 @@ func main() {
 	http.ListenAndServe(":3034", nil)
 
 }
+
+func Generar_url(parametro, host, protocolo string, url_generar map[string]string) string {
+	// Se agrega los datos de la ruta.
+	ruta_nueva, err := url.Parse(parametro)
+
+	if err != nil {
+		panic("Ocurrio Un Error")
+	}
+	ruta_nueva.Host = host
+	ruta_nueva.Scheme = protocolo
+
+	// Agregando los parametros.
+	nuevo_parametro := ruta_nueva.Query()
+
+	for key, value := range url_generar {
+		nuevo_parametro.Add(key, value)
+
+	}
+	//nuevo_parametro.Add("id", "5")
+	//nuevo_parametro.Add("nombre", "Teclados")
+
+	// Agregando a la URL el nuevo parametro creado.
+	ruta_nueva.RawQuery = nuevo_parametro.Encode()
+
+	// Imprime la url modificada
+	//log.Println(ruta_nueva.String())
+
+	return ruta_nueva.String()
+
+} // func generar_url(parametro, host, pro
